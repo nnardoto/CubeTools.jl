18a8deaaa4cd92bbcf300cf0a5035e44a866e5b8
Nathanael Nardoto Batista <nnardoto@gmail.com>
2026-06-28 19:32:49 -0300

    CubeTools.jl: estrutura inicial do pacote
    Módulo Julia para leitura e escrita de arquivos .cube (formato Gaussian).
    Inclui tipos (CubeFile, Atom), read_cube e write_cube.

---
+++ src/Types.jl
@@ -0,0 +1,22 @@
+const BohrToAngstrom = 0.529177210903
+const AngstromToBohr = 1.0 / BohrToAngstrom
+const HartreeToEV = 27.211386245988
+const EVToHartree = 1.0 / HartreeToEV
+
+
+struct Atom
+    Z        :: Int
+    charge   :: Float64
+    position :: SVector{3, Float64}
+end
+
+
+mutable struct CubeFile
+    title       :: Tuple{String, String}
+    origin      :: SVector{3, Float64}
+    dl       :: SMatrix{3, 3, Float64, 9}
+    npoints     :: SVector{3, Int}
+    periodicity :: SVector{3, Bool}
+    atoms       :: Vector{Atom}
+    data        :: Vector{Float64}
+end
