733c9991ca84316cb720fdb93c624e9a48ac57fc
Nathanael Nardoto Batista <nnardoto@gmail.com>
2026-06-30 20:15:57 -0300

    Add Display function 	modified:   main.jl 	modified:   src/CubeTools.jl 	new file:   src/Utils.jl

--- main.jl
+++ main.jl
@@ -1,5 +1,37 @@
 using CubeTools
+using LinearAlgebra
+using StaticArrays
+using Statistics
+using Printf
 
-H2 = open_cube("Telurio_bulk_model.vhart.cube")
+TelurioData = open_cube("Telurio_bulk_model.vhart.cube")
+Display(TelurioData)
 
-ElectricField(H2)
+N = length(TelurioData.atoms)
+
+
+EField = ElectricField(TelurioData)
+E = []
+
+# Obtém o módulo do campo elétrico para cada átomo
+for i in 1:N 
+    Ei = norm(EField[i])
+    push!(E, Ei)
+end
+
+# Alguma Estatística
+E_mean = mean(E)
+E_std = std(E)
+E_uncertainty = E_std/√N
+
+ExpBase = floor(log10(abs(E_mean)))
+E_mean_scaled = E_mean / 10^ExpBase
+E_uncertainty_scaled = E_uncertainty / 10^ExpBase
+println("[ElectricField] Electric Field Statistics (Bohr units)    ")
+@printf("E    = %6.4E Ha/Bohr\n", E_mean)
+@printf("σ    = %6.4E Ha/Bohr\n", E_std)
+@printf("σ/√N = %6.4E Ha/Bohr\n", E_uncertainty)
+println()
+println("[ElectricField] Electric Field with Uncertainty    ")
+
+@printf("E = (%6.4f ± %1.4f) × 10^(%+3d) Ha/Bohr\n", E_mean_scaled, E_uncertainty_scaled, ExpBase)
