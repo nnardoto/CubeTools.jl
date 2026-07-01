3f627620e95bc280004545b94178bab0360f68a0
Nathanael Nardoto Batista <nnardoto@gmail.com>
2026-06-30 21:38:58 -0300

    Alguns ajustes para trabalhar tanto em Hartree|Bohr como em eV|Angstrom

--- main.jl
+++ main.jl
@@ -5,12 +5,12 @@ using Statistics
 using Printf
 
 TelurioData = open_cube("Telurio_bulk_model.vhart.cube")
-Display(TelurioData)
+Display(TelurioData)  # Exibe informações do arquivo .cube em unidades de Angstrom
 
 N = length(TelurioData.atoms)
 
 
-EField = ElectricField(TelurioData)
+EField = ElectricField(TelurioData)  # Calcula o campo elétrico em V/Å
 E = []
 
 # Obtém o módulo do campo elétrico para cada átomo
@@ -27,11 +27,31 @@ E_uncertainty = E_std/√N
 ExpBase = floor(log10(abs(E_mean)))
 E_mean_scaled = E_mean / 10^ExpBase
 E_uncertainty_scaled = E_uncertainty / 10^ExpBase
+println("="^80)
 println("[ElectricField] Electric Field Statistics (Bohr units)    ")
 @printf("E    = %6.4E Ha/Bohr\n", E_mean)
 @printf("σ    = %6.4E Ha/Bohr\n", E_std)
 @printf("σ/√N = %6.4E Ha/Bohr\n", E_uncertainty)
 println()
 println("[ElectricField] Electric Field with Uncertainty    ")
-
 @printf("E = (%6.4f ± %1.4f) × 10^(%+3d) Ha/Bohr\n", E_mean_scaled, E_uncertainty_scaled, ExpBase)
+println("="^80)
+println()
+
+# Alguma Estatística
+E = E * HartreeToEV / BohrToAngstrom  # Converte de Hartree/Bohr para eV/Å
+E_mean = mean(E)
+E_std = std(E)
+E_uncertainty = E_std/√N
+ExpBase = floor(log10(abs(E_mean)))
+E_mean_scaled = E_mean / 10^ExpBase
+E_uncertainty_scaled = E_uncertainty / 10^ExpBase
+println("="^80)
+println("[ElectricField] Electric Field Statistics (Angstrom units)    ")
+@printf("E    = %6.4E eV/Å\n", E_mean)
+@printf("σ    = %6.4E eV/Å\n", E_std)
+@printf("σ/√N = %6.4E eV/Å\n", E_uncertainty)
+println()
+println("[ElectricField] Electric Field with Uncertainty    ")
+@printf("E = (%6.4f ± %1.4f) × 10^(%+3d) eV/Å\n", E_mean_scaled, E_uncertainty_scaled, ExpBase)
+println("="^80)
--- src/Utils.jl
+++ src/Utils.jl
@@ -3,8 +3,17 @@ function Display(cube :: CubeFile)
     a = cube.npoints[1] * cube.dl[1,:]
     b = cube.npoints[2] * cube.dl[2,:]
     c = cube.npoints[3] * cube.dl[3,:]
-   
+
+
     # Desenha uma Janela usando Printf para exibir informações do arquivo .cube
+    if cube.units == :bohr
+        println("[Summary] Units: Bohr (length) and Hartree (energy)")
+    elseif cube.units == :angstrom
+        println("[Summary] Units: Angstrom (length) and eV (energy)")
+    else
+        println("[Summary] Units: Unknown")
+    end
+    println()  
     println("+" * "-"^50 * "+")
     println(cube.LineOne)
     println(cube.LineTwo)
@@ -12,16 +21,48 @@ function Display(cube :: CubeFile)
     println("[Summary] Number of atoms: ", length(cube.atoms))
     @printf("[Summary] Data size:       %d x %d x %d\n", size(cube.data)...)
     println()
-    println("[Summary] Cell Vectors (Bohr):")
-    @printf("  a: % 9.4f % 9.4f % 9.4f\n", a[1], a[2], a[3])
-    @printf("  b: % 9.4f % 9.4f % 9.4f\n", b[1], b[2], b[3])
-    @printf("  c: % 9.4f % 9.4f % 9.4f\n", c[1], c[2], c[3])
+    if cube.units == :bohr
+        println("[Summary] Cell Vectors (Bohr):")
+        @printf("  a: % 9.4f % 9.4f % 9.4f\n", a[1], a[2], a[3])
+        @printf("  b: % 9.4f % 9.4f % 9.4f\n", b[1], b[2], b[3])
+        @printf("  c: % 9.4f % 9.4f % 9.4f\n", c[1], c[2], c[3])
+    elseif cube.units == :angstrom
+        println("[Summary] Cell Vectors (Angstrom):")
+        @printf("  a: % 9.4f % 9.4f % 9.4f\n", a[1], a[2], a[3])
+        @printf("  b: % 9.4f % 9.4f % 9.4f\n", b[1], b[2], b[3])
+        @printf("  c: % 9.4f % 9.4f % 9.4f\n", c[1], c[2], c[3])
+    end
     println()
-    println("[Summary] Atom Positions: (Bohr)")
+    if cube.units == :bohr
+        println("[Summary] Atom Positions: (Bohr):")
+    elseif cube.units == :angstrom
+        println("[Summary] Atom Positions: (Angstrom):")
+    end
     for (i, atom) in enumerate(cube.atoms)
         @printf("  Atom %d: % 9.4f % 9.4f % 9.4f\n", i, atom.position[1], atom.position[2], atom.position[3])
     end
     println("+" * "-"^50 * "+")
 
     println()
+end
+
+
+function Bohr2Angstrom(cube::CubeFile) :: CubeFile
+    buffer = copy(cube)
+    buffer.origin .= buffer.origin .* BohrToAngstrom
+    buffer.dl .= buffer.dl .* BohrToAngstrom
+    buffer.atoms = [Atom(atom.Z, atom.charge, atom.position * BohrToAngstrom) for atom in buffer.atoms]
+    buffer.data .= buffer.data .* (HartreeToEV / BohrToAngstrom)
+    buffer.units = :angstrom
+    return buffer
+end
+
+function Angstrom2Bohr(cube::CubeFile) :: CubeFile
+    buffer = copy(cube)
+    buffer.origin .= buffer.origin .* AngstromToBohr
+    buffer.dl .= buffer.dl .* AngstromToBohr
+    buffer.atoms = [Atom(atom.Z, atom.charge, atom.position * AngstromToBohr) for atom in buffer.atoms]
+    buffer.data .= buffer.data .* (EVToHartree * BohrToAngstrom)
+    buffer.units = :bohr
+    return buffer
 end
